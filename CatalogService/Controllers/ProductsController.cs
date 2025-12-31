85f238e6870e9296c6d5ff8b30ef805e1a673354
Dhruv2037 <dp2001.dd@gmail.com>
2025-12-31 16:33:15 -0500

    commit changes in catalogservice

---
+++ CatalogService/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using CatalogService.Data;
+using CatalogService.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly CatalogDbContext _db;
+
+    public ProductsController(CatalogDbContext db)
+    {
+        _db = db;
+    }
+
+    // GET: api/products
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetAll()
+    {
+        return await _db.Products.AsNoTracking().ToListAsync();
+    }
+
+    // GET: api/products/1
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Product>> GetById(int id)
+    {
+        var product = await _db.Products.FindAsync(id);
+        if (product == null)
+            return NotFound();
+
+        return product;
+    }
+
+    // POST: api/products
+    [HttpPost]
+    public async Task<ActionResult<Product>> Create(Product product)
+    {
+        _db.Products.Add(product);
+        await _db.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetById),
+            new { id = product.Id }, product);
+    }
+
+    // PUT: api/products/1
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, Product updated)
+    {
+        var product = await _db.Products.FindAsync(id);
+        if (product == null)
+            return NotFound();
+
+        product.Name = updated.Name;
+        product.Price = updated.Price;
+        product.Stock = updated.Stock;
+
+        await _db.SaveChangesAsync();
+        return NoContent();
+    }
+
+    // DELETE: api/products/1
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var product = await _db.Products.FindAsync(id);
+        if (product == null)
+            return NotFound();
+
+        _db.Products.Remove(product);
+        await _db.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ CatalogService/Data/CatalogDbContext.cs
@@ -0,0 +1,15 @@
+using CatalogService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CatalogService.Data;
+
+public class CatalogDbContext : DbContext
+{
+    public CatalogDbContext(DbContextOptions<CatalogDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+}
